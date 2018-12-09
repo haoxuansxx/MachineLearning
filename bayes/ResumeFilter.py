e365a7fcffb21f2ff74d0d827f2c71d46517a881
haoxuansxx <sunxuexin1991@163.com>
2018-12-09 14:38:31 +0800

    简历过滤器

---
+++ bayes/ResumeFilter.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# __author__ = "Sun"
+import pandas as pd
+import numpy as np
+
+# degree:学历、education:毕业院校、skills:技能、working_experience:曾经工作过的公司、position:当前职位
+data = pd.read_excel("employees_dataset.csv")
